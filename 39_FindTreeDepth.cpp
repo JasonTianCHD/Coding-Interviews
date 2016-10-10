a910f9353c85a6f411a6dfb1b44f56cf6756dcda
JasonTianCHD <641817405@qq.com>
2016-10-10 22:04:35 +0800

    Update and rename FindTreeDepth to 39_FindTreeDepth.cpp

--- 39_FindTreeDepth.cpp
+++ 39_FindTreeDepth.cpp
@@ -1,14 +1,15 @@
 //找到二叉树的深度  递归、非递归 两种方法
+// 特殊情况：NULL；
 
 // 递归版
 
-int MaxDepth(BiTree *BT){
+int MaxDepth(BiTree* BT){
     int depth = 0;
-    if(BT)
+    if(BT)//若不为空，则递归的查找左右子树的深度
     {
         int LeftDepth = MaxDepth(BT->lchild);
         int RightDepth = MaxDepth(BT->rchild);
-        depth = LeftDepth>=RightDepth?LeftDepth+1:RightDepth+1;
+        depth = (LeftDepth>=RightDepth)?(LeftDepth+1):(RightDepth+1);
     }
     return depth;
 }
